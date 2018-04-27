d5837c078861f349883c480d0cfea2f908e8b829
Kristen GARNIER <kgarnier@iris-interactive.fr>
2018-04-27 09:49:52 +0200

    exercice 3

---
+++ filter/sorting/src/main/main.controller.js
@@ -0,0 +1,34 @@
+angular
+	.module('main')
+	.controller('MainController', MainController)
+
+function MainController() {
+	this.order = 'title';
+	this.reverse = false;
+	this.movies = [
+		{
+			title: 'Jurrasic park',
+			year: 1993
+		},
+		{
+			title: 'l\'Odyssée de l\'espace',
+			year: 1968
+		},
+		{
+			title: 'Le géant de fer',
+			year: 1995
+		},
+		{
+			title: 'Avatar',
+			year: 2009
+		},
+		{
+			title: 'Sharknado',
+			year: 2013
+		}
+	] // On utilise la syntaxe Controller as donc on utilise this
+
+	this.setOrder = function(order) {
+		this.order = order;
+	}
+}
